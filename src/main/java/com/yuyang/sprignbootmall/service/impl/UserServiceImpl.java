40f89a96f83c52919864385510245d4da37f3915
YuYang <the38245691>
2022-10-04 09:16:50 +0800

    帳號功能 - 檢查註冊EMAIL、隱藏返回的密碼

--- src/main/java/com/yuyang/sprignbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/yuyang/sprignbootmall/service/impl/UserServiceImpl.java
@@ -4,17 +4,31 @@ import com.yuyang.sprignbootmall.dao.UserDao;
 import com.yuyang.sprignbootmall.dto.UserRegisterRequest;
 import com.yuyang.sprignbootmall.model.User;
 import com.yuyang.sprignbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查email是否已註冊過
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+        log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+
+        if( user != null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //註冊新email
         return userDao.createUser(userRegisterRequest);
     }
 
